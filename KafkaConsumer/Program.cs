256b7a0d91edf4a04290aa17cb129840ce8d4a77
Guilherme Hott <guilherme@clout.com>
2017-05-03 18:36:41 -0700

    Simple Kafka Consumer

---
+++ KafkaConsumer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using KafkaNet;
+using KafkaNet.Model;
+
+namespace KafkaConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = new KafkaOptions(new Uri("http://localhost:9092")
+                );
+            var router = new BrokerRouter(options);
+            var consumer = new KafkaNet.Consumer(new ConsumerOptions("TestTopic",
+               new BrokerRouter(options)));
+
+            //Consume returns a blocking IEnumerable (ie: never ending stream)
+            foreach (var message in consumer.Consume())
+            {
+                Console.WriteLine("Response: P{0},O{1} : {2}",
+                   message.Meta.PartitionId, message.Meta.Offset,
+                   Encoding.UTF8.GetString(message.Value));
+            }
+        }
+    }
+}
